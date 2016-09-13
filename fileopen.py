b0bc14598d96ff730fff5711e989b26ffeb3b462
Karan <karansharma1295@gmail.com>
2016-09-13 20:54:26 +0530

    file open in python

--- fileopen.py
+++ fileopen.py
@@ -1,3 +1,7 @@
 with open("blahblah.txt") as myFile:
     for lines in myFile:
         print (lines) # or do some other thing with the line...
+
+
+with open('newfile.txt', 'a') as the_file:
+	the_file.write('Hello from the other side\n')
